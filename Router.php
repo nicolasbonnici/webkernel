f1ab4ea6260a9a2886808ba3b7fcf5fbd4122f7a
Nicolas Bonnici <nicolasbonnici@gmail.com>
2014-04-11 20:58:33 +0200

    Modification de la config du Router (a terme en config.json)

--- Router.php
+++ Router.php
@@ -51,7 +51,7 @@ class Router extends Singleton
 
     public static function init()
     {
-        
+
         // @todo retrieve from db/config/overwrite
         self::$aRules = array(
             '/login' => array(
@@ -79,10 +79,10 @@ class Router extends Singleton
                 'controller' => 'home',
                 'action' => 'about'
             ),
-            '/lifestream' => array(
-                'bundle' => 'lifestream',
+            '/activities' => array(
+                'bundle' => 'frontend',
                 'controller' => 'home',
-                'action' => 'index'
+                'action' => 'activities'
             )
         // '/blog' => array(
         // 'bundle' => 'blog',
@@ -95,30 +95,30 @@ class Router extends Singleton
         // 'action' => 'index'
         // )
                 );
-        
+
         self::$sUrl = $_SERVER['REQUEST_URI'];
-        
+
         self::$aRequest = self::cleanArray(explode('/', self::$sUrl)); // @todo move function cleanArray to toolbox
-        
+
         self::$sLang = self::DEFAULT_LOCALE;
         self::$sBundle = self::DEFAULT_BUNDLE;
         self::$sController = self::DEFAULT_CONTROLLER;
         self::$sAction = self::DEFAULT_ACTION;
-        
+
         if (is_array(self::$aRequest) && count(self::$aRequest) > 0) {
-            
+
             // Test custom routing here
             self::matchRules();
         }
-        
+
         foreach ($_FILES as $key => $value) {
             self::$aParams[$key] = $value;
         }
-        
+
         foreach ($_POST as $key => $value) {
             self::$aParams[$key] = $value;
         }
-        
+
         foreach ($_GET as $key => $value) {
             self::$aParams[$key] = $value;
         }
@@ -127,27 +127,27 @@ class Router extends Singleton
     private static function matchRules()
     {
         assert('is_array(self::$aRequest) && count(self::$aRequest)>0');
-        
+
         // @see flag cstom route found
         $bRouted = false;
-        
+
         foreach (self::$aRules as $sUrl => $aRule) {
-            
+
             // @see custom routing rule match with request
             $aUrl = explode(':', $sUrl);
             if (preg_match('#^/' . self::$aRequest[0] . '#', $aUrl[0])) {
-                
+
                 assert('is_array($aRule)');
-                
+
                 $bRouted = false;
-                
+
                 self::$sBundle = self::$aRules[$sUrl]['bundle'];
                 self::$sController = self::$aRules[$sUrl]['controller'];
                 self::$sAction = self::$aRules[$sUrl]['action'];
                 if (($aParams = array_slice(self::$aRequest, count(self::cleanArray(explode('/', $aUrl[0]))))) && count($aParams) > 0) {
                     self::setParams($aParams);
                 }
-                
+
                 return;
             }
         }
@@ -158,22 +158,22 @@ class Router extends Singleton
                 if (isset(self::$aRequest[0])) {
                     self::$sBundle = self::$aRequest[0];
                 }
-                
+
                 if (isset(self::$aRequest[1])) {
                     self::$sController = self::$aRequest[1];
                 }
-                
+
                 if (isset(self::$aRequest[2])) {
                     self::$sAction = self::$aRequest[2];
                 }
-                
+
                 self::setParams(array_slice(self::$aRequest, 3));
             }
         }
-        
+
         return;
     }
-    
+
     // @todo c'est deguelasse y'a array_values pour faire ce genre de traitement
     private static function cleanArray(array $aArray = array())
     {
@@ -190,7 +190,7 @@ class Router extends Singleton
     /**
      * Parse parameters from request url
      *
-     * @param array $items            
+     * @param array $items
      * @return array
      */
     private static function setParams(array $items)
